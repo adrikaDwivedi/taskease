12c4182849e41d6f7a13b819c5bc22b8be851dc3
AdrikaDwivedi <89826992+adrikaDwivedi@users.noreply.github.com>
2025-06-24 10:25:50 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBWUVZ8bIGjNdrIAmmbkrDpPxIISWHNxPc",
   authDomain: "whatbytes-4bcba.firebaseapp.com",
   projectId: "whatbytes-4bcba",
-  storageBucket: "whatbytes-4bcba.appspot.com",  // ✅ corrected
+  storageBucket: "whatbytes-4bcba.appspot.com",  
   messagingSenderId: "795619802442",
   appId: "1:795619802442:web:85d07aecae8357a66095b3"
 };
@@ -16,24 +16,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth }; // ✅ cleaner
+export { db, auth }; // 
 
 
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBWUVZ8bIGjNdrIAmmbkrDpPxIISWHNxPc",
-//   authDomain: "whatbytes-4bcba.firebaseapp.com",
-//   projectId: "whatbytes-4bcba",
-//   storageBucket: "whatbytes-4bcba.firebasestorage.app",
-//   messagingSenderId: "795619802442",
-//   appId: "1:795619802442:web:85d07aecae8357a66095b3"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
